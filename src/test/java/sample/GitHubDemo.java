3d252c868d49594ba779c197da06a68d5fc3c61f
geswe <geswe@Swetha>
2025-03-28 09:42:33 +0530

    M2-Demo

--- src/test/java/sample/GitHubDemo.java
+++ src/test/java/sample/GitHubDemo.java
@@ -1,5 +1,15 @@
 package sample;
 
-public class GitHubDemo {
+import org.testng.annotations.Test;
+
+public class GitHubDemo 
+{
+	@Test
+	public void m1() 
+	{
+		System.out.println("Hello");
+		System.out.println("Hi...");
+		
+	}
 
 }
